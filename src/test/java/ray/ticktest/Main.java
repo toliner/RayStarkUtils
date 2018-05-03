44b64f7ff24d9ce16a011816c9867ef97d1421e9
RayStarkMC <raystarkmc@gmail.com>
2018-05-03 18:18:57 +0900

    テストコード改変

--- src/test/java/ray/ticktest/Main.java
+++ src/test/java/ray/ticktest/Main.java
@@ -5,8 +5,18 @@ import ray.util.tick.TickManager;
 
 public class Main {
     public static void main(String[] args){
+        testTPS();
+    }
+
+    private static void testTickManager(){
         TickManager manager = new TickManager(1000);
-        manager.add(new TickTest());
+        manager.add(new ITickControlled() {
+            int i=0;
+            @Override
+            public void done(){
+                System.out.println(i++);
+            }
+        });
 
         try {
             manager.start();
@@ -22,11 +32,31 @@ public class Main {
         }
     }
 
-    private static class TickTest implements ITickControlled {
-        int i=0;
-        @Override
-        public void done(){
-            System.out.println(i++);
+    private static void testTPS(){
+        TickManager manager = new TickManager(20);
+        manager.add(new ITickControlled() {
+            long oldTick = manager.getCurrentTick();
+            @Override
+            public void done() {
+                if(20 * (manager.getCurrentTick()-oldTick) < 1000) return;
+
+                System.out.println(manager.getTPS());
+                oldTick = manager.getCurrentTick();
+            }
+        });
+
+        try {
+            manager.start();
+            Thread.sleep(10000);
+            manager.stop();
+            System.out.println("TPS: " + manager.getTPS() + "This TPS must be 0.");
+            Thread.sleep(3000);
+            manager.start();
+            Thread.sleep(10000);
+            manager.stop();
+
+        }catch(InterruptedException e){
+            e.printStackTrace();
         }
     }
 }
