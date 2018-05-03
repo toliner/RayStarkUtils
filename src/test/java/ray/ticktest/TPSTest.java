9bcea6b33f9c868e719966f5e76241148903a119
トリナー <toliner.rkt@gmail.com>
2018-05-04 01:13:50 +0900

    TPSのtestにassertを使用

--- src/test/java/ray/ticktest/TPSTest.java
+++ src/test/java/ray/ticktest/TPSTest.java
@@ -22,7 +22,8 @@ public class TPSTest {
             manager.start();
             Thread.sleep(10000);
             manager.stop();
-            System.out.println("TPS:" + manager.getTPS() + " This TPS must be 0.");
+            long tps = manager.getTPS();
+            assert tps == 0 : "current tps:" + tps;
             Thread.sleep(3000);
             manager.start();
             Thread.sleep(10000);
