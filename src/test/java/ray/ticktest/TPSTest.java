8d343a8c87443fdd6c1b27c705d68246ebc51b88
RayStarkMC <raystarkmc@gmail.com>
2018-05-03 19:41:19 +0900

    テスト内容ごとにクラスを分離

--- src/test/java/ray/ticktest/TPSTest.java
+++ src/test/java/ray/ticktest/TPSTest.java
@@ -3,36 +3,8 @@ package ray.ticktest;
 import ray.util.tick.ITickControlled;
 import ray.util.tick.TickManager;
 
-public class Main {
-    public static void main(String[] args){
-        testTPS();
-    }
-
-    private static void testTickManager(){
-        TickManager manager = new TickManager(1000);
-        manager.add(new ITickControlled() {
-            int i=0;
-            @Override
-            public void done(){
-                System.out.println(i++);
-            }
-        });
-
-        try {
-            manager.start();
-            Thread.sleep(5000);
-            manager.stop();
-            Thread.sleep(3000);
-            manager.start();
-            Thread.sleep(5000);
-            manager.stop();
-
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }
-    }
-
-    private static void testTPS(){
+public class TPSTest {
+    private static void main(String[] args){
         TickManager manager = new TickManager(20);
         manager.add(new ITickControlled() {
             long oldTick = manager.getCurrentTick();
@@ -49,7 +21,7 @@ public class Main {
             manager.start();
             Thread.sleep(10000);
             manager.stop();
-            System.out.println("TPS: " + manager.getTPS() + "This TPS must be 0.");
+            System.out.println("TPS:" + manager.getTPS() + " This TPS must be 0.");
             Thread.sleep(3000);
             manager.start();
             Thread.sleep(10000);
@@ -60,4 +32,3 @@ public class Main {
         }
     }
 }
-
---
+++ src/test/java/ray/ticktest/TickManagerTest.java
@@ -0,0 +1,32 @@
+package ray.ticktest;
+
+import ray.util.tick.ITickControlled;
+import ray.util.tick.TickManager;
+
+public class TickManagerTest {
+
+    public static void main(String[] args){
+        TickManager manager = new TickManager(1000);
+        manager.add(new ITickControlled() {
+            int i=0;
+                @Override
+                public void done(){
+                    System.out.println(i++);
+                }
+            });
+
+        try {
+            manager.start();
+            Thread.sleep(5000);
+            manager.stop();
+            Thread.sleep(3000);
+            manager.start();
+            Thread.sleep(5000);
+            manager.stop();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
